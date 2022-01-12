a470c33dd2e513b2778cf1b96ec8247c900d2557
Ncihanoglu <nihatcihanoglu@Nihat-MacBook-Pro.local>
2022-01-12 18:26:33 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const inputRef = useRef(null);
+  const onSubitHandler = (event) => {
+    event.preventDefault();
+    console.error('cant add validation to uncontrol');
+    alert('uncontrolled:' + inputRef.current.value);
+    alert('controlled:' + inputValue);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App-header">
+      <h1>Uncontrol</h1>
+      <form
+        onSubmit={onSubitHandler}
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <input ref={inputRef} />
+        <button
+          type="submit"
+          disabled={inputRef.current && inputRef.current.value.length < 3}
         >
-          Learn React
-        </a>
-      </header>
+          Submit With Validation
+        </button>
+      </form>
+      <hr style={{ width: '100%' }} />
+      <h1>Control</h1>
+      <form
+        onSubmit={onSubitHandler}
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <input
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button type="submit" disabled={inputValue.length < 3}>
+          Submit With Validation
+        </button>
+      </form>
     </div>
   );
 }
